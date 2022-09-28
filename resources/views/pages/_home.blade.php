a2481c7088332e84194cb47ddf922091d3ad1705
Tomasz Tomżyński <tomasz.tomzynski@gmail.com>
2022-09-28 12:08:58 +0200

    changes table view

--- resources/views/pages/_home.blade.php
+++ resources/views/pages/_home.blade.php
@@ -16,10 +16,10 @@
             <div class="col-12 col-lg-8">
                 <div class="header-new__box">
                     <h1>Balumed - Specjalistyczne Centrum Medyczne w Warszawie</h1>
-                                        <p class="description">Specjalistyczne Centrum Medyczne Profilaktyki, Diagnostyki i Terapii Małego Dziecka powstało z myślą o zapewnieniu pełnej i wysokospecjalistycznej opieki zdrowotnej najmłodszym pacjentom już od momentu ich poczęcia. Nasza działalność skupiona jest na takich dziedzinach medycyny jak <a href="/pediatria">pediatria</a>, <a href="/neonantologia">neonatologia</a> oraz położnictwo z <a href="/ginekologia">ginekologią</a>. Ciąże fizjologiczne, a także ciąże wysokiego ryzyka położniczego prowadzone są przez wysokiej klasy specjalist&oacute;w położnictwa i ginekologii z dużym doświadczeniem klinicznym.</p>
-                                    <div class="btn-wrapper">
-                                        <a href="{{route('contact')}}" class="btn btn-primary">Skontaktuj się z nami</a>
-                                    </div>
+                        <p class="description">Specjalistyczne Centrum Medyczne Profilaktyki, Diagnostyki i Terapii Małego Dziecka powstało z myślą o zapewnieniu pełnej i wysokospecjalistycznej opieki zdrowotnej najmłodszym pacjentom już od momentu ich poczęcia. Nasza działalność skupiona jest na takich dziedzinach medycyny jak <a href="/pediatria">pediatria</a>, <a href="/neonantologia">neonatologia</a>.</p>
+                        <div class="btn-wrapper">
+                            <a href="{{route('contact')}}" class="btn btn-primary">Skontaktuj się z nami</a>
+                        </div>
                 </div>
             </div>
         </div>
