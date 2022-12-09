e663830772ff0b742c25be91410b67caece1505a
Tomasz Tomżyński <tomasz.tomzynski@gmail.com>
2022-12-09 11:02:40 +0100

    add auth to upload

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,12 @@ Route::get('/galeria', 'HomeController@galery')->name('galery');
 
 Route::get('/', 'HomeController@index')->name('home');
 
-Route::get('/admin', 'HomeController@admin')->name('adminHome')->middleware('auth');;
+Route::get('/admin', 'HomeController@admin')->name('adminHome')->middleware('auth');
+;
 
 //Blog routes
-Route::get('/admin/blog', 'BlogController@adminBlog')->name('adminBlog')->middleware('auth');;
+Route::get('/admin/blog', 'BlogController@adminBlog')->name('adminBlog')->middleware('auth');
+;
 Route::resource('blog', 'BlogController')->parameters([
     'blog' => 'slug'
 ]);
@@ -33,53 +35,62 @@ Route::resource('blog', 'BlogController')->parameters([
 
 //Services routes
 
-Route::get('/admin/uslugi', 'ServiceController@adminService')->name('adminService')->middleware('auth');;
+Route::get('/admin/uslugi', 'ServiceController@adminService')->name('adminService')->middleware('auth');
+;
 Route::resource('uslugi', 'ServiceController')->parameters([
     'usługi' => 'slug'
 ])->except(['show']);
 
 
 //Specialist routes
-Route::get('/admin/specjalisci', 'SpecialistController@adminSpecialist')->name('adminSpecialist')->middleware('auth');;
+Route::get('/admin/specjalisci', 'SpecialistController@adminSpecialist')->name('adminSpecialist')->middleware('auth');
+;
 Route::resource('specjalisci', 'SpecialistController')->parameters([
     'specjalisci' => 'slug'
 ]);
 
 //Posts routes
-Route::get('/admin/aktualnosci', 'PostController@adminPost')->name('adminPost')->middleware('auth');;
+Route::get('/admin/aktualnosci', 'PostController@adminPost')->name('adminPost')->middleware('auth');
+;
 Route::resource('aktualnosci', 'PostController')->parameters([
     'aktualnosci' => 'slug'
 ]);
 
 //Tags routes
-Route::get('/admin/tagi', 'TagController@adminTag')->name('adminTag')->middleware('auth');;
+Route::get('/admin/tagi', 'TagController@adminTag')->name('adminTag')->middleware('auth');
+;
 Route::resource('tagi', 'TagController')->parameters([
     'tagi' => 'slug'
-])->middleware('auth');;
+])->middleware('auth');
+;
 
 //Info routes
-Route::get('/admin/info', 'InfoController@adminInfo')->name('adminInfo')->middleware('auth');;
-Route::resource('info', 'InfoController')->only(['update'])->middleware('auth');;
+Route::get('/admin/info', 'InfoController@adminInfo')->name('adminInfo')->middleware('auth');
+;
+Route::resource('info', 'InfoController')->only(['update'])->middleware('auth');
+;
 
 //Partner routes
-Route::get('/admin/partnerzy', 'PartnerController@adminPartner')->name('adminPartner')->middleware('auth');;
-Route::resource('partnerzy', 'PartnerController')->only(['update'])->middleware('auth');;
+Route::get('/admin/partnerzy', 'PartnerController@adminPartner')->name('adminPartner')->middleware('auth');
+;
+Route::resource('partnerzy', 'PartnerController')->only(['update'])->middleware('auth');
+;
 
 
 
 
-Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
+Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['auth']], function () {
     \UniSharp\LaravelFilemanager\Lfm::routes();
 });
-Route::get('/admin/pliki',  function () {
+Route::get('/admin/pliki', function () {
     return view('admin.files.index');
 })->name('adminFiles')->middleware('auth');
 
-Route::get('/kontakt',  function () {
+Route::get('/kontakt', function () {
     return view('pages._contact');
 })->name('contact');
 
-Route::get('/harmonogram-pracy-lekarzy',  function () {
+Route::get('/harmonogram-pracy-lekarzy', function () {
     return view('pages._shedule');
 })->name('shedule');
 
